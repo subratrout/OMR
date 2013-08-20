9983826f95e44a0bd21844d49a012575dd726224
Subrat Rout <subratnrout@gmail.com>
2013-08-19 22:59:02 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr::Application.config.secret_key_base = 'a8d9dd9b12df601784ec3672d3894c10106b2cff8d87c96a7fe3889568f384b1d9d7a16b44575292956dd1798deda3fa19f7c865de0d3856a712907d20069f09'
