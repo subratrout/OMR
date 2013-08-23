f7d011aa5a1c9da9eb6362611086bf5a9a400a8d
Subrat Rout <subratnrout@gmail.com>
2013-08-22 22:51:23 -0700

    Added Pins associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
          #:recoverable, 
          :rememberable, :trackable, :validatable
 
-    validates_presence_of :name
+    has_many :pins, :dependent => :destroy
 end
